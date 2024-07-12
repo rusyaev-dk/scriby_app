2fa652520d91c54524bde8dda1e6fa1fa48f47f0
Denis Rusyaev <rusyaev.dk@gmail.com>
2024-07-12 14:01:41 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:scriby_app/scriby_app.dart';
+
+void main() {
+  runApp(const ScribyApp());
+}
---
+++ lib/scriby_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScribyApp extends StatelessWidget {
+  const ScribyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
